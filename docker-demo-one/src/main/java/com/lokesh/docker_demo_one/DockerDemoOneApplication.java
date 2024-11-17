c4c82fada3efbf2ae33a39ae4268c0d148d544da
Lokesh Mittal <lokesh.adonis@gmail.com>
2024-11-17 16:49:41 -0500

    Initial commit

---
+++ docker-demo-one/src/main/java/com/lokesh/docker_demo_one/DockerDemoOneApplication.java
@@ -0,0 +1,13 @@
+package com.lokesh.docker_demo_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDemoOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDemoOneApplication.class, args);
+	}
+
+}
---
+++ docker-demo-one/src/test/java/com/lokesh/docker_demo_one/DockerDemoOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lokesh.docker_demo_one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerDemoOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
